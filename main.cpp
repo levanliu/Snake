2cb25d4e03c66b1ed4fe7244f1e7dd0488197365
levanliu <839119912@qq.com>
2023-11-14 22:26:12 +0800

    add

---
+++ main.cpp
@@ -0,0 +1,157 @@
+﻿#include <iostream>
+#include <conio.h>
+// Library effective with Windows
+#include <windows.h>
+
+// Library effective with Linux
+//#include <unistd.h>
+
+using namespace std;
+bool gameOver;
+
+const int width = 20;
+const int height = 20;
+
+int x, y,fruitX,fruitY,score;
+
+enum eDirection
+{
+    STOP = 0, LEFT,RIGHT,UP,DOWN
+};
+
+eDirection dir;
+
+void Setup()
+{
+    gameOver = false;
+
+    dir = STOP;
+    x = width / 2;
+    y = height / 2;
+    fruitX = rand() % width;
+    fruitY = rand() % height;
+
+    score = 0;
+}
+
+void Draw()
+{
+    system("cls"); //system("clear");
+    for (int i = 0; i < width+2; i++)
+    {
+        cout << "#";
+    }
+    cout << endl;
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            if (j == 0) {
+                cout << "#";
+            }
+
+            if (i == y && j == x)
+                cout << "O";
+            else if (i == fruitY && j == fruitX)
+                cout << "F";
+            else
+                cout << " ";
+            
+
+            if (j == width - 1) {
+                cout << "#";
+            }
+        }
+        cout << endl;
+    }
+
+    for (int i = 0; i < width+2; i++)
+    {
+        cout << "#";
+    }
+    cout << endl;
+    cout << "Score: " << score << endl;
+}
+void Input()
+{
+    if (_kbhit())
+    {
+        switch (_getch())
+        {
+        case 'a':
+            dir = LEFT;
+            break;
+        case 'd':
+            dir = RIGHT;
+            break;
+        case 'w':
+            dir = UP;
+            break;
+        case 's':
+            dir = DOWN;
+            break;
+        case 'x':
+            gameOver = true;
+            break;
+        default:
+            break;
+        }
+    }
+}
+void Logic()
+{
+    switch (dir)
+    {
+        case LEFT:
+            x--;
+            break;
+        case RIGHT:
+            x++;
+            break;
+        case UP:
+            y--;
+            break;
+        case DOWN:
+            y++;
+            break;
+        default:
+            break;
+    }
+
+    if (x > width)
+        x -= width;
+    if (x < 0)
+        x += width;
+    if (y > height)
+        y -= height;
+    if (y < 0)
+        y += height;
+
+    if (x > width || x < 0 || y >height || y < 0)
+        gameOver = true;
+    if (x == fruitX && y == fruitY)
+    {
+        score += 10;
+        fruitX = rand() % width;
+        fruitY = rand() % height;
+    }
+}
+
+int main()
+{
+    Setup();
+    while (!gameOver) {
+        Draw();
+        Input();
+        Logic();
+
+        if (dir != 0) {
+            Sleep(5);
+        }
+        Sleep(100);
+           
+    }
+    std::cout << "Hello World!\n";
+}
+
